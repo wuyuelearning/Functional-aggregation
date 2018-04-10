e6e42ec9ca3c49807350211ac1f2834704de3027
wuyue <wuyue@lvmama.com>
2018-04-10 19:44:16 +0800

    两种测量屏幕宽高的方法

--- app/src/main/java/Common/PopupWindowDown3.java
+++ app/src/main/java/Common/PopupWindowDown3.java
@@ -2,8 +2,12 @@ package Common;
 
 import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 
@@ -20,6 +24,9 @@ public class PopupWindowDown3 extends PopupWindow {
     private boolean openAlpha;
     private ValueAnimator valueAnimator;
 
+    private int width;
+    private int height;
+
     public PopupWindowDown3(Activity context, View view) {
         super(context);
         initPopupWindowStyle(context, view);
@@ -28,8 +35,10 @@ public class PopupWindowDown3 extends PopupWindow {
     private void initPopupWindowStyle(Activity context, View view) {
         // 设置弹窗填充布局
         setContentView(view);
-        int width = context.getWindowManager().getDefaultDisplay().getWidth();
-        int height = context.getWindowManager().getDefaultDisplay().getHeight();
+
+        getScreenWidthHeight1(context);
+//        getScreenWidthHeight2(context);
+
         // 设置弹窗的宽
         setWidth(width / 2 - 100);
         // 设置弹窗的高
@@ -54,10 +63,11 @@ public class PopupWindowDown3 extends PopupWindow {
     }
 
     /**
-     *  设置弹窗的偏移量
-     * @param anchor  在anchor固定控件下弹出
-     * @param xoff  相对x 轴的偏移量
-     * @param yoff 相对y 轴的偏移量
+     * 设置弹窗的偏移量
+     *
+     * @param anchor 在anchor固定控件下弹出
+     * @param xoff   相对x 轴的偏移量
+     * @param yoff   相对y 轴的偏移量
      */
 
     @Override
@@ -65,19 +75,19 @@ public class PopupWindowDown3 extends PopupWindow {
         super.showAsDropDown(anchor, xoff, yoff);
     }
 
-    private  void startAlphaAnim(){
-        if(!openAlpha){
-            return ;
+    private void startAlphaAnim() {
+        if (!openAlpha) {
+            return;
         }
-        if(valueAnimator != null){
+        if (valueAnimator != null) {
             valueAnimator.start();
         }
 
         valueAnimator = new ValueAnimator();
         valueAnimator.setDuration(300);
         //  设置动画变化浮动值
-        valueAnimator.setFloatValues(0,128);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+        valueAnimator.setFloatValues(0, 128);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 int alpha = (int) animation.getAnimatedValue();
@@ -89,10 +99,37 @@ public class PopupWindowDown3 extends PopupWindow {
     }
 
     // 是否打开设置背景色
-    public void setOpenAlpha(boolean b){
+    public void setOpenAlpha(boolean b) {
         openAlpha = b;
-        if(openAlpha){
+        if (openAlpha) {
             setBackgroundDrawable(new ColorDrawable(0xff000000));
         }
+    }
+
+    private void getScreenWidthHeight1(Activity context) {
+
+        width = context.getWindowManager().getDefaultDisplay().getWidth();
+        height = context.getWindowManager().getDefaultDisplay().getHeight();
+    }
+
+    private void getScreenWidthHeight2(Activity context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+
+        width = dm.widthPixels;
+        height = dm.heightPixels;
+        float density = dm.density;
+        float densityDpi = dm.densityDpi;
+
+        int screenWidth = (int) (width / density);
+        int screenHeight = (int) (height / density);
+
+        Log.d("h_bl", "屏幕宽度（像素）：" + width);
+        Log.d("h_bl", "屏幕高度（像素）：" + height);
+        Log.d("h_bl", "屏幕密度（0.75 / 1.0 / 1.5）：" + density);
+        Log.d("h_bl", "屏幕密度dpi（120 / 160 / 240）：" + densityDpi);
+        Log.d("h_bl", "屏幕宽度（dp）：" + screenWidth);
+        Log.d("h_bl", "屏幕高度（dp）：" + screenHeight);
     }
 }
