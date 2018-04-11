a328f7a36b96698658f0d2fc87a7cd9c7bd12b19
wuyue <wuyue@lvmama.com>
2018-04-11 11:23:39 +0800

    将计算屏幕宽高的方法放在一个类里面

---
+++ app/src/main/java/Utils/GetScreenWidthHeight.java
@@ -0,0 +1,112 @@
+package Utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+/**
+ * Created by admin on 2018/4/11.
+ */
+
+public class GetScreenWidthHeight {
+
+    int width;
+    int height;
+    Activity context ;
+
+    public void getScreenWidthHeight1() {
+        WindowManager wm = context.getWindowManager();
+        width = wm.getDefaultDisplay().getWidth();
+        height = wm.getDefaultDisplay().getHeight();
+    }
+
+
+    public void getScreenWidthHeight2() {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        width = wm.getDefaultDisplay().getWidth();
+        height = wm.getDefaultDisplay().getHeight();
+    }
+
+
+    public void getScreenWidthHeight3() {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+
+        width = dm.widthPixels;
+        height = dm.heightPixels;
+        float density = dm.density;
+        float densityDpi = dm.densityDpi;
+
+        int screenWidth = (int) (width / density);
+        int screenHeight = (int) (height / density);
+
+        Log.d("h_bl", "屏幕宽度（像素）：" + width);
+        Log.d("h_bl", "屏幕高度（像素）：" + height);
+        Log.d("h_bl", "屏幕密度（0.75 / 1.0 / 1.5）：" + density);
+        Log.d("h_bl", "屏幕密度dpi（120 / 160 / 240）：" + densityDpi);
+        Log.d("h_bl", "屏幕宽度（dp）：" + screenWidth);
+        Log.d("h_bl", "屏幕高度（dp）：" + screenHeight);
+
+
+    }
+
+    public void getScreenWidthHeight4() {
+        Resources resources = context.getResources();
+        DisplayMetrics dc = resources.getDisplayMetrics();
+
+        width = dc.widthPixels;
+        height = dc.heightPixels;
+
+        float density = dc.density;
+        float densityDpi = dc.densityDpi;
+
+
+        int screenWidth = (int) (width / density);
+        int screenHeight = (int) (height / density);
+
+        Log.d("h_bl", "屏幕宽度（像素）：" + dc.widthPixels);
+        Log.d("h_bl", "屏幕高度（像素）：" + dc.heightPixels);
+        Log.d("h_bl", "屏幕密度（0.75 / 1.0 / 1.5）：" + density);
+        Log.d("h_bl", "屏幕密度dpi（120 / 160 / 240）：" + densityDpi);
+        Log.d("h_bl", "屏幕宽度（dp）：" + screenWidth);
+        Log.d("h_bl", "屏幕高度（dp）：" + screenHeight);
+
+    }
+
+
+
+    public int getScreenWidth() {
+        return width;
+    }
+
+    public int getScreenHeight(){
+        return height;
+    }
+    public void selectFunc(Activity context,int i){
+        this.context=context;
+        switch (i){
+            case 1:
+                getScreenWidthHeight1();
+                break;
+            case 2:
+                getScreenWidthHeight2();
+                break;
+            case 3:
+                getScreenWidthHeight3();
+                break;
+            case 4:
+                getScreenWidthHeight4();
+                break;
+            default:
+                getScreenWidthHeight1();
+        }
+    }
+
+}
+
+
+
