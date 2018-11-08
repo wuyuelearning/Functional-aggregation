94f6cd2af9bf629f16179e2de03d72d4fa9364e9
wuyue <wuyue@lvmama.com>
2018-11-08 15:21:16 +0800

    备忘录模式-修改

--- app/src/main/java/memorandum/MemorandumFragment.java
+++ app/src/main/java/memorandum/MemorandumFragment.java
@@ -1,6 +1,6 @@
 package memorandum;
 
-import android.graphics.Canvas;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
--- app/src/main/java/memorandum/NoteEditText.java
+++ app/src/main/java/memorandum/NoteEditText.java
@@ -1,10 +1,8 @@
 package memorandum;
-
-
-
 import android.content.Context;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
+import android.widget.EditText;
 
 /**
  * Created by wuyue on 2018/10/26.
@@ -13,19 +11,55 @@ import android.util.AttributeSet;
 
 public class NoteEditText extends AppCompatEditText {
 
+    // extends EditText 可以输入，第二个就能输入
+//    public NoteEditText(Context context) {
+//        super(context);
+//    }
 
-    public NoteEditText(Context context) {
-        this(context,null);
-    }
+//    public NoteEditText(Context context, AttributeSet attrs) {
+//        super(context, attrs);
+//    }
 
+//    public NoteEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//    }
+
+    //  extends EditText 不能输入
+//    public NoteEditText(Context context) {
+//        this(context,null);
+//    }
+//
+//    public NoteEditText(Context context, AttributeSet attrs) {
+//        this(context, attrs,0);
+//    }
+//
+//    public NoteEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//    }
+
+//  extends AppCompatEditText 可以输入，只要第二个即可
+//    public NoteEditText(Context context) {
+//        super(context);
+//    }
     public NoteEditText(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        super(context, attrs);
     }
+//    public NoteEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//    }
 
-    public NoteEditText(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
+    // extends AppCompatEditText  不能输入
+//    public NoteEditText(Context context) {
+//        this(context,null);
+//    }
+//
+//    public NoteEditText(Context context, AttributeSet attrs) {
+//        this(context, attrs,0);
+//    }
+//
+//    public NoteEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//    }
     //  创建备忘录对象，即存储编辑器的指定数据
     public Memote createMemoto(){
         Memote memote =new Memote();
